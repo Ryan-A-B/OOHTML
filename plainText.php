b3501e9c78969511bc0fb49e292d40e26b95c7b6
Ryan Ballinger <ryan.a.ballinger@gmail.com>
2015-06-06 12:40:06 +1000

    Amended the way argument type of string is enforced in constructor

--- plainText.php
+++ plainText.php
@@ -2,7 +2,10 @@
 	class PlainText extends Content {
 		private $text;
 
-		public function __construct (string $text) {
+		public function __construct ($text) {
+            if (!is_string($text)) {
+                throw new Exception('PlainText __construct expects string');
+            }
 			$this->text = $text;
 		}
 

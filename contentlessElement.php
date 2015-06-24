a2c231b082fc1bbef94c31b03f5d3fe9330847c9
Ryan Ballinger <ryan.a.ballinger@gmail.com>
2015-06-24 21:47:44 +1000

    Custom Elements and Default Attributes added

--- contentlessElement.php
+++ contentlessElement.php
@@ -1,33 +1,84 @@
 <?php
     require_once('content.php');
 
-	class ContentlessElement extends Content {
-		protected $attributes;
-		protected $tag;
+    if (!class_exists('ContentlessElement')) {
+        class ContentlessElement extends Content {
+            // Static Members and Functions
+            protected static $customElements;
+            protected static $defaultAttributes;
 
-		public function __construct ($tag) {
-			$this->attributes = array();
-			$this->tag = $tag;
-		}
+            public static function init () {
+                static::$customElements = array();
+                static::$defaultAttributes = array();
+            }
 
-        public function __set ($attribute, $value) {
-            $this->attributes[$attribute] = $value;
-        }
+            public static function createCustomElement ($custom_tag, $tag, array $defaultAttributes = array()) {
+                static::$customElements[$custom_tag] = $tag;
 
-		public function generateHTML () {
-			$html = "<{$this->tag}";
-            foreach ($this->attributes as $attribute => $value) {
-                if (is_bool($value)) {
-                    if ($value === true) {
-                        $html .= " $attribute";
-                    }
-                } else {
-				    $html .= " $attribute='$value'";
+                if (!empty($defaultAttributes)) {
+                    static::addDefaultAttributes($custom_tag, $defaultAttributes);
                 }
-			}
-			$html .= '>';
+            }
 
-			return $html;
-		}
-	}
+            public static function addDefaultAttribute ($tag, $attribute, $value) {
+                if (!isset(static::$defaultAttributes[$tag])) {
+                    static::$defaultAttributes[$tag] = array();
+                }
+
+                if (is_null($value)) {
+                    unset(static::$defaultAttributes[$tag][$attribute]);
+                } else {
+                    static::$defaultAttributes[$tag][$attribute] = $value;
+                }
+            }
+
+            public static function addDefaultAttributes ($tag, array $attributes) {
+                foreach ($attributes as $attribute => $value) {
+                    static::addDefaultAttribute($tag, $attribute, $value);
+                }
+            }
+
+            // Non-Static Members and Functions
+            protected $attributes;
+            protected $tag;
+
+            public function __construct ($tag, array $attributes = array()) {
+                if (isset(static::$defaultAttributes[$tag])) {
+                    $attributes = array_merge(static::$defaultAttributes[$tag], $attributes);
+                }
+
+                if (isset(static::$customElements[$tag])) {
+                    $tag = static::$customElements[$tag];
+                }
+
+                $this->attributes = $attributes;
+                $this->tag = $tag;
+            }
+
+            public function __set ($attribute, $value) {
+                if (is_null($value)) {
+                    unset($this->attributes[$attribute]);
+                } else {
+                    $this->attributes[$attribute] = $value;
+                }
+            }
+
+            public function generateHTML () {
+                $html = "<{$this->tag}";
+                foreach ($this->attributes as $attribute => $value) {
+                    if (is_bool($value)) {
+                        if ($value === true) {
+                            $html .= " $attribute";
+                        }
+                    } else {
+                        $html .= " $attribute='$value'";
+                    }
+                }
+                $html .= '>';
+
+                return $html;
+            }
+        }
+        ContentlessElement::init();
+    }
 ?>
--- element.php
+++ element.php
@@ -1,49 +1,55 @@
 <?php
     require_once('contentlessElement.php');
 
-	class Element extends ContentlessElement {
-		private $content;
+    if (!class_exists('Element')) {
+        class Element extends ContentlessElement {
+            protected static $customElements;
+            protected static $defaultAttributes;
 
-		public function __construct ($tag) {
-            parent::__construct($tag);
+            private $content;
 
-			$this->content = array();
-		}
+            public function __construct ($tag, array $attributes = array()) {
+                parent::__construct($tag, $attributes);
 
-		public function createElement ($tag) {
-			$element = new Element($tag);
-			$this->content[] = $element;
-			return $element;
-		}
+                $this->content = array();
+            }
 
-		public function createContentlessElement ($tag) {
-			$element = new ContentlessElement($tag);
-			$this->content[] = $element;
-			return $element;
-		}
+            public function createElement ($tag, array $attributes = array()) {
+                $element = new Element($tag, $attributes);
+                $this->content[] = $element;
+                return $element;
+            }
 
-        public function prepend (Content $content) {
-            array_unshift($this->content, $content);
+            public function createContentlessElement ($tag, array $attributes = array()) {
+                $element = new ContentlessElement($tag, $attributes);
+                $this->content[] = $element;
+                return $element;
+            }
+
+            public function prepend (Content $content) {
+                array_unshift($this->content, $content);
+            }
+
+            public function append (Content $content) {
+                $this->content[] = $content;
+            }
+
+            public function addContent (Content $content) {
+                $this->content[] = $content;
+            }
+
+            public function generateHTML () {
+                $html = parent::generateHTML();
+
+                foreach ($this->content as $element) {
+                    $html .= $element->generateHTML();
+                }
+
+                $html .= "</{$this->tag}>";
+
+                return $html;
+            }
         }
-
-        public function append (Content $content) {
-            $this->content[] = $content;
-        }
-
-		public function addContent (Content $content) {
-			$this->content[] = $content;
-		}
-
-		public function generateHTML () {
-            $html = parent::generateHTML();
-
-			foreach ($this->content as $element) {
-				$html .= $element->generateHTML();
-			}
-
-			$html .= "</{$this->tag}>";
-
-			return $html;
-		}
-	}
+        Element::init();
+    }
 ?>

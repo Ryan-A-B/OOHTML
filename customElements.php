8c6d27b819bfe3de5ac541d195a4eb78aadb5b55
Ryan Ballinger <ryan.a.ballinger@gmail.com>
2015-08-22 21:15:17 +1000

    Added more custom elements

--- customElements.php
+++ customElements.php
@@ -7,6 +7,35 @@
         'class' => 'row'
     ]);
 
+    Element::createCustomElement('navbar', 'nav', [
+        'class' => 'navbar navbar-default'
+    ]);
+
+    Element::createCustomElement('navbar-header', 'div', [
+        'class' => 'navbar-header'
+    ]);
+
+    Element::createCustomElement('navbar-collapse', 'div', [
+        'class' => 'collapse navbar-collapse'
+    ]);
+
+    Element::createCustomElement('navbar-list', 'ul', [
+        'class' => 'nav navbar-nav'
+    ]);
+
+    Element::createCustomElement('button-group', 'div', [
+        'class' => 'btn-group',
+        'role' => 'group'
+    ]);
+
+    Element::createCustomElement('button-default', 'div', [
+        'class' => 'btn btn-default'
+    ]);
+
+    Element::createCustomElement('icon-bar', 'span', [
+        'class' => 'icon-bar'
+    ]);
+
     Element::createCustomElement('carousel', 'div', [
         'class' => 'carousel slide',
         'date-ride' => 'carousel'
